7241c7a2840a35952de8af5762f4579a8e644ea1
tupurp <tupurpcheung@gmail.com>
2020-06-01 01:32:29 +0800

    springcloud 使用zookeeper作为服务注册中心

---
+++ cloud-provider-payment-8004/src/main/java/com/tupurp/springcloud/PaymentMain8004.java
@@ -0,0 +1,16 @@
+package com.tupurp.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description:
+ * @author: tupurp
+ * @create: 2020-06-01 00:39
+ */
+@SpringBootApplication
+public class PaymentMain8004 {
+    public static void main(String[] args){
+        SpringApplication.run(PaymentMain8004.class,args);
+    }
+}
