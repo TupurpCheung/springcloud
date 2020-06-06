a89bdec081fe891c234f42959f5641dacffacdee
tupurp <tupurpcheung@gmail.com>
2020-06-06 23:09:54 +0800

    springcloud hystrix 通用服务降级接口实现（注意：eureka服务注册中心挂掉一段时间内，接口也是可以调通的）

--- cloud-consumer-feign-hystrix-order-80/src/main/java/com/tupurp/springcloud/service/PaymentHystrixService.java
+++ cloud-consumer-feign-hystrix-order-80/src/main/java/com/tupurp/springcloud/service/PaymentHystrixService.java
@@ -9,9 +9,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 /**
  *
  * feign的接口，测试hystrix
+ * 并指定兜底的服务降级实现类
+ * feign底层依赖ribbon，feign实现了动态代理拼接Http请求
+ * 而ribbon将eureka注册中的数据缓存到了本地，即使eureka服务注册中心挂掉，在一定时间内也可以依靠本地缓存找到服务接口实现调用
  * */
 @Component
-@FeignClient(value = "CLOUD-PROVIDER-HYSTRIX-PAYMENT")
+@FeignClient(value = "CLOUD-PROVIDER-HYSTRIX-PAYMENT",fallback = PaymentHystrixFallbackService.class)
 public interface PaymentHystrixService {
 
     @GetMapping("/payment/hystrix/ok/{id}")
