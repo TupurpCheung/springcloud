6f5ec673f3f4a9f982fc102928acaa280ea5da12
tupurp <tupurpcheung@gmail.com>
2020-06-07 23:29:53 +0800

    springcloud 配置中心客户端手动刷新

---
+++ cloud-config-center-3355/src/main/java/com/tupurp/springcloud/ConfigClientMain3355.java
@@ -0,0 +1,18 @@
+package com.tupurp.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @description:
+ * @author: tupurp
+ * @create: 2020-06-07 23:11
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3355 {
+    public static void main(String[] args){
+        SpringApplication.run(ConfigClientMain3355.class,args);
+    }
+}
