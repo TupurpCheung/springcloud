f247ee928721d69cdc55886a0ad53df07dfb6465
tupurp <tupurpcheung@gmail.com>
2020-06-08 00:09:03 +0800

    springcloud 配置中心使用Bus动态刷新全局广播配置

---
+++ cloud-config-client-3366/src/main/java/com/tupurp/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,29 @@
+package com.tupurp.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @description:
+ * @author: tupurp
+ * @create: 2020-06-07 23:34
+ */
+@RestController
+//刷新注解 发送Post请求至 http://localhost:3366/actuator/refresh可刷新，必须是Post
+@RefreshScope
+public class ConfigClientController {
+
+    // 因为config仓库以rest形式暴露，所以所有客户端都可以通过config服务端访问到github上对应的文件信息
+    @Value("${config.info}")
+    private String configInfo;
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo() {
+        return "serverPort: " + serverPort + "\t\n\n configInfo" + configInfo;
+    }
+}
