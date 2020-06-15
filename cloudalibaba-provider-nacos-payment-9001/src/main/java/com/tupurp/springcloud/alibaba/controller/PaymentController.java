666041935233b8b0026e5e8dcd8f092fef1356c1
tupurp <tupurpcheung@gmail.com>
2020-06-15 23:45:48 +0800

    springcloud alibaba nacos 服务提供者注册

---
+++ cloudalibaba-provider-nacos-payment-9001/src/main/java/com/tupurp/springcloud/alibaba/controller/PaymentController.java
@@ -0,0 +1,23 @@
+package com.tupurp.springcloud.alibaba.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @description:
+ * @author: tupurp
+ * @create: 2020-06-15 23:21
+ */
+@RestController
+public class PaymentController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @GetMapping(value = "/payment/nacos/{id}")
+    public String getPayment(@PathVariable("id") Long id){
+        return "nacos resigry,serverPort :" + serverPort + "\t id :" + id;
+    }
+}
---
+++ cloudalibaba-provider-nacos-payment-9002/src/main/java/com/tupurp/springcloud/alibaba/PaymentMain9002.java
@@ -0,0 +1,18 @@
+package com.tupurp.springcloud.alibaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @description:
+ * @author: tupurp
+ * @create: 2020-06-15 23:39
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentMain9002 {
+    public static void main(String[] args){
+        SpringApplication.run(PaymentMain9002.class,args);
+    }
+}
